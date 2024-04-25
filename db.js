e531cb26bc134fe61a6ed823395dcac69dc6adfa
Abhijeet Mishra <abhijeet.mishra1003@gmail.com>
2024-04-25 12:45:51 +0530

    shift mongo db to online

--- db.js
+++ db.js
@@ -1,13 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+// including .env file
+require('dotenv').config();
 
 // Define the MongoDB connection URL
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels' // Replace 'mydatabase' with your database name
+
+//           local mongoose
+// const mongoURL = process.env.DB_URL_LOCAL // Replace 'mydatabase' with your database name
+
+//            online mongoose
+const mongoURL = process.env.DB_URL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Get the default connection
 // Mongoose maintains a default connection object representing the MongoDB connection.
@@ -15,18 +23,17 @@ const db = mongoose.connection;
 
 // Define event listeners for database connection
 
-db.on('connected', () => {
-    console.log('Connected to MongoDB server');
+db.on("connected", () => {
+  console.log("Connected to MongoDB server");
 });
 
-db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
 });
 
-db.on('disconnected', () => {
-    console.log('MongoDB disconnected');
+db.on("disconnected", () => {
+  console.log("MongoDB disconnected");
 });
 
 // Export the database connection
-module.exports = db
-
+module.exports = db;
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 
+
+// calling .env file
+require('dotenv').config();
+
+
 const bodyParser = require('body-parser');  //jo bhi data backend pr aarha hoga body parser use jason format me convert kar deta hai
 app.use(bodyParser.json()); // req.body
-
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.send('Welcome to our Hotel');
@@ -18,7 +23,10 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
+
+
+
 //   comment added again so that we can push in github
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('listening on port 3000');
 })
